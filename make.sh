b00b20b38e844a5139158d173ea62a07a03f28aa
Kenny <lstkenny@gmail.com>
2020-02-02 21:57:19 +0700

    Add creating zip archive of extension directory

--- make.sh
+++ make.sh
@@ -8,9 +8,14 @@ rm -r $dest/
 fi #[ -d "../$1" ]
 mkdir $dest
 echo "copying files"
-rsync -arv --exclude=.git --exclude=index.html --exclude=make.sh --exclude=manifest* --exclude=readme* ./ $dest
+rsync -arvq --exclude=.git --exclude=index.html --exclude=make.sh --exclude=manifest* --exclude=readme* ./ $dest
 echo "copying manifest"
 cp "manifest.json.$1" $dest/manifest.json
+pushd $dest > /dev/null
+echo "creating zip archive"
+zip -r -q $1.zip .
+popd > /dev/null
+echo "done"
 fi #[ $1 = "chrome" ] || [ $1 = "opera" ] || [ $1 = "firefox" ]
 fi #[ $# -gt 0 ]
 
