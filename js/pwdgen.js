4f23bc61252af1e8de20aaed8d044b6ce9f64860
kenny <kenny@pinkclicks.com>
2020-02-04 14:50:24 +0700

    Optimize data manipulation; Fix hostname and root domain detection; Refactor long methods;

--- js/pwdgen.js
+++ js/pwdgen.js
@@ -9,7 +9,7 @@ class PwdGen extends DataSet {
 	constructor(state) {
 		super()
 		this.storage = new Storage()
-		this.cookie = new Cookie()
+		this.cookie = new Cookie("http://pwdgenextension")
 		this.srand = new SRand()
 	}
 	init() {
@@ -34,47 +34,17 @@ class PwdGen extends DataSet {
 				return Promise.resolve()
 			})
 	}
-	saveState() {
-		const pwg = {}
-		pwg.config = this.clone("config")
-		//	save secret
-		switch (this.get("config.rememberSecret")) {
-			case "session" :
-				this.cookie.set("_pwg_secret", this.get("data.secret"), this.get("config.sessionHours", 1))
-				break
-			case "remember" :
-				pwg.config.secret = this.get("data.secret")
-				break
-			default :
-				pwg.config.secret = false
-		}
-		//	save options
-		if (this.get("config.rememberSettings")) {
-			const data = {}
-			const options = ["len", "digits", "lower", "upper", "symbols", "subdomains", "exclude", "similar"]
-			options.forEach((value, key) => {
-				if (!this.get("data." + value)) {
-					data[value] = false
-				} else {
-					data[value] = this.data[value]
-				}
-			})
-			pwg.default = data
-			this.set("default", data)
-			if (this.get("data.domain") && this.get("config.rememberSettings") == "domains") {
-				pwg.domains = this.clone("domains", {})
-				pwg.domains[this.get("data.domain").replace(/\./g, "_")] = data
-				this.set("domains", pwg.domains)
-			}
-		}
-		this.storage.set("pwg", pwg)
+	getData(key) {
+		return this.get("data." + key)
 	}
-	extractHostName(href) {
+	getConfig(key) {
+		return this.get("config." + key)
+	}
+	getHostName(href) {
 		const url = new URL(href)
 		return url.hostname.toLowerCase()
 	}
-	extractRootDomain(url) {
-		let domain = this.extractHostName(url)
+	getRootDomain(domain) {
 		let domainParts = domain.split(".")
 		const partsLength = domainParts.length
 		let partsCount = 2
@@ -91,6 +61,105 @@ class PwdGen extends DataSet {
 		}
 		return domainParts.slice(Math.max(partsLength - partsCount, 0)).join(".")
 	}
+	excludeChars(arr, exclude) {
+		let symbols = arr.join("")
+		exclude.forEach(symbol => {
+			symbols = symbols.replace(symbol, "")
+		})
+		return symbols.split("")
+	}
+	getRandomSeed(min, max) {
+		return Math.floor(Math.random() * (max - min)) + min
+	}
+	getSeed() {
+		const parts = []
+		if (this.get("data.secret")) {
+			parts.push(this.get("data.secret"))
+		}
+		if (this.get("data.subdomains")) {
+			parts.push(this.get("data.host").toLowerCase())
+		} else {
+			parts.push(this.get("data.domain").toLowerCase())
+		}
+		return parts.join(".")
+	}
+	getChars() {
+		const chars = {
+			"digits": "0123456789".split(""),
+			"lower": "abcdefghijklmnopqrstuvwxyz".split(""),
+			"upper": "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""),
+			"symbols": "!@#$%^&*/-+=()<>[]{}_\\|.,:;'\"`~".split("")
+		}
+		let exclude = this.get("data.exclude")
+		if (this.get("data.similar")) {
+			exclude += "1iIlL0oO"
+		}
+		exclude = exclude.split("")
+		for (let key in chars) {
+			if (this.get("data." + key)) {
+				chars[key] = this.excludeChars(chars[key], exclude)
+				if (chars[key].length) {
+					chars[key] = this.srand.rndArrayShuffle(chars[key])
+				} else {
+					delete chars[key]
+				}
+			} else {
+				delete chars[key]
+			}
+		}
+		return chars	
+	}
+	saveSecret() {
+		switch (this.get("config.rememberSecret")) {
+			case "session" :
+				this.cookie.set("_pwg_secret", this.get("data.secret"), this.get("config.sessionHours", 1))
+				break
+			case "remember" :
+				this.set("config.secret", this.get("data.secret"))
+				break
+			default :
+				this.set("config.secret", null)
+		}
+	}
+	escapeDomain(domain) {
+		return domain.replace(/\./g, "_")
+	}
+	getDomainOptions(domain) {
+		domain = this.escapeDomain(domain)
+		return this.clone("domains." + domain, this.get("default"))
+	}
+	saveDomainOptions(domain, data) {
+		if (domain && this.get("config.rememberSettings") == "domains") {
+			domain = this.escapeDomain(domain)
+			this.set("domains." + domain, data)
+		}
+	}
+	saveOptions() {
+		if (this.get("config.rememberSettings")) {
+			const data = {}
+			const options = ["len", "digits", "lower", "upper", "symbols", "subdomains", "exclude", "similar"]
+			options.forEach((value, key) => {
+				let optionValue = this.get("data." + value)
+				if (!optionValue) {
+					data[value] = false
+				} else {
+					data[value] = optionValue
+				}
+			})
+			this.set("default", data)
+			this.saveDomainOptions(this.get("data.domain"), data)
+		}
+	}
+	saveState() {
+		this.saveSecret()
+		this.saveOptions()
+		let pwg = {
+			"config": this.clone("config"),
+			"default": this.clone("default"),
+			"domains": this.clone("domains")
+		}
+		this.storage.set("pwg", pwg)
+	}
 	setOptions(options) {
 		for (let key in options) {
 			this.set("data." + key, options[key])
@@ -106,70 +175,24 @@ class PwdGen extends DataSet {
 					break
 			}
 			if (!secret) {
-				secret = this.randomSeed(100000, 999999)
+				secret = this.getRandomSeed(100000, 999999)
 			}
 			this.set("data.secret", secret)
 		}
 	}
 	setUrl(url) {
-		const domain = this.extractRootDomain(url)
-		const options = this.clone("domains." + domain.replace(/\./g, "_"), this.get("default"))
+		const host = this.getHostName(url)
+		const domain = this.getRootDomain(host)
+		const options = this.getDomainOptions(domain)
 		options.url = url
 		options.domain = domain
-		options.host = this.extractHostName(url)
+		options.host = host
 		this.setOptions(options)
-	}
-	randomSeed(min, max) {
-		return Math.floor(Math.random() * (max - min)) + min
-	}
-	getSeed() {
-		const parts = []
-		if (this.get("data.secret")) {
-			parts.push(this.get("data.secret"))
-		}
-		if (this.get("data.subdomains")) {
-			parts.push(this.get("data.host").toLowerCase())
-		} else {
-			parts.push(this.get("data.domain").toLowerCase())
-		}
-		return parts.join(".")
-	}
-	getData(key) {
-		return this.get("data." + key)
-	}
-	getConfig(key) {
-		return this.get("config." + key)
-	}
-	arrayExclude(arr, exclude) {
-		let symbols = arr.join("")
-		exclude.forEach(symbol => {
-			symbols = symbols.replace(symbol, "")
-		})
-		return symbols.split("")
 	}
 	updatePassword() {
 		this.srand.seed(this.getSeed())
-		const chars = {
-			"digits": "0123456789".split(""),
-			"lower": "abcdefghijklmnopqrstuvwxyz".split(""),
-			"upper": "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""),
-			"symbols": "!@#$%^&*/-+=()<>[]{}_\\|.,:;'\"`~".split("")
-		}
-		let exclude = this.get("data.exclude")
-		if (this.get("data.similar")) {
-			exclude += "1iIlL0oO"
-		}
-		exclude = exclude.split("")
-		let scope = []
-		for (let key in chars) {
-			if (this.get("data." + key)) {
-				chars[key] = this.arrayExclude(chars[key], exclude)
-				chars[key] = this.srand.rndArrayShuffle(chars[key])
-				if (chars[key].length) {
-					scope.push(key)
-				}
-			}
-		}
+		const chars = this.getChars()
+		const scope = Object.keys(chars)
 		let pwd = []
 		for (let i = 0; i < this.get("data.len"); i++) {
 			pwd.push(this.srand.rndArrayValue(chars[scope[(i + scope.length) % scope.length]]))
